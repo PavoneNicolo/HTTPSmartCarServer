219b1d25ddaf784fc4a76357b5a11844d3d239ed
Nicolò Pavone <nicolo.pavone@tecnicosuperiorekennedy.it>
2018-11-22 19:27:50 +0100

    struttura migliorata con l'utilizzo delle routes

--- data_process/influx_utility.js
+++ data_process/influx_utility.js
@@ -3,18 +3,15 @@ module.exports = {
         let result = "";
         let fieldsLength = data.fields.length;
 
-        //ciclo tutte le misure
+        //cycle all fields and format correctly
         for (let i = 0; i < fieldsLength; i++) {
-            //forma valore per formato JSON
             let value = '"' + data.fields[i].fldName + '":' + data.fields[i].value;
             result = result + value + ",";
         }
 
-        //utile ad eliminare l'ultima virgola prima di inserire in influx
         result = result.substring(0, result.length - 1);
 
         return JSON.parse('{' + result + '}');
-        //return "{\"temperature\":" + data.Temperature + ",\"speed\":" + data.Speed + ",\"gpsLat\":" + data.Gps.lat + ",\"gpsLon\":" + data.Gps.lon + ",\"direction\":\"" + data.Direction + "\"}";
     },
     writeInflux: function (db, data) {
         let influxFields = this.getFields(data);
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const restify = require('restify');
+const Router = require('restify-router').Router;
+const routerInstance = new Router();
 const server = restify.createServer({name: "HTTP API"});
-const config = require('./config');
-const process = require('./data_process');
-const db = config.influxConfig('localhost', 'cars_data', 8086);
+const httpRoute = require('./protocol/HTTP/index.js');
+
+httpRoute.applyRoutes(server, '/http');
 
 server.use(restify.plugins.bodyParser());
-
-//utile per ricevere coda, elaborare dati e scriverli su influx
-server.post('/cars/:vinNumber/data', function (req, res, next) {
-    let data = JSON.parse(req.body);
-    data.vinNumber = req.params.vinNumber;
-
-    try {
-        process.writeInflux(db, data);
-        res.send(200);
-    }
-    catch (e) {
-        res.send(500);
-    }
-
-    return next();
-});
 
 server.listen(8080, function () {
     console.log('%s listening at localhost', server.name);
